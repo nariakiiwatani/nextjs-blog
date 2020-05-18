49da06ac09eeaf8c67498584681bdce7e1a2ef35
nariakiiwatani <nariakiiwatani@annolab.com>
2020-05-18 16:06:33 +0900

    parse markdown to html

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,17 +1,23 @@
+import Head from "next/head"
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
 	return (
-	  <Layout>
-		{postData.title}
-		<br />
-		{postData.id}
-		<br />
-		{postData.date}
-	  </Layout>
-	)
-  }
+		<Layout>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+		  {postData.title}
+		  <br />
+		  {postData.id}
+		  <br />
+		  {postData.date}
+		  <br />
+		  <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+		</Layout>
+	  )
+}
 export async function getStaticPaths() {
 	const paths = getAllPostIds()
 	return {
@@ -21,7 +27,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-	const postData = getPostData(params.id)
+	const postData = await getPostData(params.id)
 	return {
 	  props: {
 		postData
